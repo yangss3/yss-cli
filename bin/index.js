05df6b2d521eee7596b6cb0d122e82e5d522dada
yangss3 <yss_2016@outlook.com>
2020-07-18 15:07:40 +0800

    update

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 const { program } = require('commander')
-
+const enhanceErrorMessages = require('../lib/utils/enhanceErrorMessages')
 program
-  .version(require('../package.json').version)
+  .version(`yss-cli v${require('../package.json').version}`)
+  .name('yss')
   .usage('<command> [options]')
 
 program
   .command('create <projectName>')
   .description('Create a project template')
-  .action((projectName) => {
+  .action(projectName => {
     require('../lib/create')(projectName)
   })
 
+enhanceErrorMessages()
 
 program.parse(process.argv)
